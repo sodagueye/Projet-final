6900d7344e44ec90a8fa3e6de684408d86b74c44
DELL LATITUDE 7470 <gueyesoda56@gmail.com>
2024-05-30 13:52:16 +0000

    modification2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Dashboard from "./Dashboard/Dashboard";
 import Navbar from "./Dashboard/Navbar";
 import Test from "./Dashboard/Test";
 import "./styles/style.css";
+import Inscrire from "./Inscription/Inscrire";
+import Connexion from "./Inscription/Connexion";
+import MotdepassOublié from "./Inscription/MotdepassOublié";
 function App() {
   return (
     <div>
@@ -15,6 +18,9 @@ function App() {
           <Route path="/" />
           <Route path="/Admin" element={<Dashboard />} />
           <Route path="/Test" element={<Test />} />
+          <Route path="/connexion" element={<Connexion/> }></Route>
+          <Route path="/inscrire" element={<Inscrire/>}></Route>
+          <Route path="/reinitialiser" element={ <MotdepassOublié/>}></Route>
           <Route
             path="/About"
             element={
