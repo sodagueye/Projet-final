74f5f9cb932485ba55fda8a79d04e4874dcc36d5
Khadija <dija5631@gmail.com>
2024-06-04 14:50:11 +0200

    fusion reussi

--- src/Inscription/Inscrire.jsx
+++ src/Inscription/Inscrire.jsx
@@ -1,41 +1,39 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 import '../Inscription/inscrire.css';
-import { Link } from 'react-router-dom';
+import { useNavigate,Link } from 'react-router-dom';
 import axios from 'axios';
 
 function Inscrire() {
-
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
+  const history=useNavigate();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const [telephone, setTelephone] = useState('');
+  const [number, setNumber] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmpassword, setConfirmpassword] = useState('');
-  // const[tableau ,setTableau]=useState([]);
+  const [confirmPassword, setConfirmPassword] = useState('');
+  
+async function submit(e) {
+  e.preventDefault();
 
+  try {
+      const res = await axios.post("http://localhost:8000/users/register", {
+          firstName,
+          lastName,
+          email,
+          number,
+          password,
+          confirmPassword
+      });
 
-
-  useEffect(() => {
-    if (firstname && lastname && email && telephone && password && password === confirmpassword) {
-      const data = {
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
-        telephone: telephone,
-        password: password
-      };
-      axios.post('https://localhost:8000/users/register', data)
-        .then(response => {
-
-          console.log('Inscription réussie:', response.data);
-          // Rediriger l'utilisateur vers une autre page par exemple '/connexion'
-        })
-        .catch(error => {
-
-          console.error('Erreur lors de l\'inscription:', error);
-        });
-    }
-  }, [firstname, lastname, email, telephone, password, confirmpassword]);
+      if (res.data === "exist") {
+          alert("existe deja");
+      } else if (res.data === "exist pas") {
+      }
+  } catch (error) {
+      // alert("Error");
+      console.log(error);
+  }
+}
 
 
   return (
@@ -50,19 +48,19 @@ function Inscrire() {
  
           
 
-              <input class=" input " type="text" placeholder="Prenom" required value={firstname} onChange={(e) => {
-                setFirstname(e.target.value);
+              <input class=" input " type="text" placeholder="Prenom" required value={firstName} onChange={(e) => {
+                setFirstName(e.target.value);
                 console.log("hello")
               }} />
            
-              <input class=" input  number" type="text" placeholder="Nom" value={lastname} required onChange={e => setLastname(e.target.value)} />
+              <input class=" input  number" type="text" placeholder="Nom" value={lastName} required onChange={e => setLastName(e.target.value)} />
            
           </div>
           <div className='d-flex  gap-4 justify-content-center inscript'>
         
               <input class=" input " type="email" placeholder="Email" value={email} required onChange={e => setEmail(e.target.value)} />
           
-              <input class=" input number" type="number" placeholder="Telephone" required value={telephone} onChange={e => setTelephone(e.target.value)} />
+              <input class=" input number" type="number" placeholder="Telephone" required value={number} onChange={e => setNumber(e.target.value)} />
           
           </div>
           <div className='d-flex  gap-4  justify-content-center inscript pass'>
@@ -70,15 +68,15 @@ function Inscrire() {
               <input class=" input " type="text" placeholder="Mot de pass" required value={password} onChange={e => setPassword(e.target.value)} />
            
            
-              <input class=" input " type="text" placeholder="Confirmation mot de pass" required value={confirmpassword} onChange={e => {
-                setConfirmpassword(e.target.value)
-                console.log(confirmpassword);
+              <input class=" input " type="text" placeholder="Confirmation mot de pass" required value={confirmPassword} onChange={e => {
+                setConfirmPassword(e.target.value)
+                console.log(confirmPassword);
               }} />
          
           </div>
           <p className='m-3 text-center'>By clicking "Submit," you agree to <span className='terms'> E-delivery General Terms and Conditions </span> and acknowledge you have read the  <span className='terms'>Privacy Policy.</span></p>
           <div className=' creer '>
-            <Link to='/connexion' className='liens'> <button type='submit' className='creer liens fs-5 fw-bold' >Creer un compte</button></Link>
+            <Link to='/connexion' className='liens'> <button onClick={submit} type='submit' className='creer liens fs-5 fw-bold' >Creer un compte</button></Link>
           </div>
         </form>
 
