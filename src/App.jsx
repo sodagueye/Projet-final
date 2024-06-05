dbba45c1278a730a26d1edcb4b9b936f328a27f8
DELL LATITUDE 7470 <gueyesoda56@gmail.com>
2024-06-05 12:03:24 +0000

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Connexion from './Inscription/Connexion';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MotdepassOublié from './Inscription/MotdepassOublié';
 import ListeUtilisateur from './Inscription/ListeUtilisateur';
+import ModifierPassword from './Inscription/ModifierPassword';
 
 function App(){
 
@@ -19,6 +20,7 @@ function App(){
           <Route path="/" element={<Inscrire/>}></Route>
           <Route path="/inscrire" element={<Inscrire/>}></Route>
           <Route path="/reinitialiser" element={ <MotdepassOublié/>}></Route>
+          <Route path="/modifier" element={ <ModifierPassword/>}></Route>
           <Route path="/listerUsers" element={ <ListeUtilisateur/>}></Route>
 
         </Routes>
