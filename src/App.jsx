c572e310a529c5052c5ec397e1b82cd912e74d25
Your fatoubeyeyaye <yayefatoubeye@g.com>
2024-06-06 09:31:20 +0000

    affichage panier

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Connexion from './Inscription/Connexion';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Connexion from './Inscription/Connexion';
 import MotdepassOublié from './Inscription/MotdepassOublié';
 import ListeUtilisateur from './Inscription/ListeUtilisateur';
 import ModifierPassword from './Inscription/ModifierPassword';
-import Inscrire from'./Inscription/Inscrire';
-
+import Inscrire from './Inscription/Inscrire';
 import Menu from './Componentnav/Menu';
-function App(){
+import AffichagePanier from './Componentnav/AffichagePanier';
+//import { Table } from 'react-bootstrap'; 
 
+function App() {
   return (
     <div className="App">
-    
-    <BrowserRouter>
+      <BrowserRouter>
+        <Menu />
         <Routes>
-
-          <Route path="/connexion" element={<Connexion/> } />
-          <Route path="/" element={<Inscrire/>}></Route>
-          <Route path="/inscrire" element={<Inscrire/>}></Route>
-          <Route path="/reinitialiser" element={ <MotdepassOublié/>}></Route>
-          <Route path="/modifier" element={ <ModifierPassword/>}></Route>
-          <Route path="/listerUsers" element={ <ListeUtilisateur/>}></Route>
-          
-
+          <Route exact path="/connexion" element={<Connexion />} />
+          <Route path="/inscrire" element={<Inscrire />} />
+          <Route path="/reinitialiser" element={<MotdepassOublié />} />
+          <Route path="/modifier" element={<ModifierPassword />} />
+          <Route path="/listerUsers" element={<ListeUtilisateur />} />
+          <Route path="/panier" element={<AffichagePanier />} /> 
+          {/* Ajoutez d'autres routes si nécessaire */}
+          <Route path="/" element={<Home />} /> 
         </Routes>
       </BrowserRouter>
-     <Menu/>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Bienvenue à la page d'accueil</h2>; 
 }
 
 export default App;
