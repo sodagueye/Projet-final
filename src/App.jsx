45660ece82730515234969e0ef5aeca763e3a04e
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2024-06-10 23:17:06 +0200

    switch to Admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReservationPage from "./Reservation/Reservation";
-// import Appreciation from "./Appreciation/Appreciation";
-// import ReservationTable from "./Reservation-Table/ReservationTable";
 import Inscrire from "./Inscription/Inscrire";
-// import Connexion from "./Inscription/Connexion";
-// import MotdepassOublié from "./Inscription/MotdepassOublié";
 import Menu from "./Componentnav/Menu";
 import Accueil from "./khadyComponent/Accueil/Accueil"
 import Dashboard from "./Dashboard/Dashboard";
@@ -17,8 +13,6 @@ function App() {
           <Route path="/reservation" element={<ReservationPage />} />
           <Route path="/inscription" element={<Inscrire />} />
           <Route path="/" element={<Accueil />} />
-          {/* <Route path="/MotdepassOublié" element={<MotdepassOublié />} /> */}
-          {/* <Route path="/appreciation" element={<Appreciation />} /> */}
           <Route path="/admin" element={<Dashboard />} />
           <Route path="/menu" element={<Menu />} />
         </Routes>
