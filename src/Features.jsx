8c908400cf6d5ff41eac71c194f03bfe73c65f4f
ZooM982 <haurlyroll@gmail.com>
2024-06-11 17:25:42 +0000

    test

---
+++ src/Features.jsx
@@ -0,0 +1,11 @@
+import { GiEyelashes } from "react-icons/gi";
+
+export const Features = () => {
+  return (
+    <section>
+      <div id="features" className="my-5">
+        <h1>Revhieno vous salut  <GiEyelashes /></h1>
+      </div>
+    </section>
+  );
+};
