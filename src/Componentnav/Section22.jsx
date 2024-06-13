28893fb7f2d019a069cb4e6577a2a8c86aec4b52
Your fatoubeyeyaye <yayefatoubeye@g.com>
2024-06-13 17:25:21 +0000

    integration des icones  dans les bouton du categorie

--- src/Componentnav/Section22.jsx
+++ src/Componentnav/Section22.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { FaHamburger, FaIceCream, FaUtensils, FaCocktail, FaListAlt } from 'react-icons/fa';
 import '../Componentnav/section22.css';
 
 const Section22 = ({ onCategorySelect }) => {
   return (
     <div>
-      <button onClick={() => onCategorySelect('')} className='b1'>Tous</button>
-      <button onClick={() => onCategorySelect('burger')} className='b1'>Burger</button>
-      <button onClick={() => onCategorySelect('glace')} className='b1'>Glace</button>
-      <button onClick={() => onCategorySelect('plat')} className='b1'>Plats</button>
-      <button onClick={() => onCategorySelect('jus')} className='b1'>Jus</button>
+      <button onClick={() => onCategorySelect('')} className='b1'>
+        <FaListAlt /> Tous
+      </button>
+      <button onClick={() => onCategorySelect('burger')} className='b1'>
+        <FaHamburger /> Burger
+      </button>
+      <button onClick={() => onCategorySelect('glace')} className='b1'>
+        <FaIceCream /> Glace
+      </button>
+      <button onClick={() => onCategorySelect('plat')} className='b1'>
+        <FaUtensils /> Plats
+      </button>
+      <button onClick={() => onCategorySelect('jus')} className='b1'>
+        <FaCocktail /> Jus
+      </button>
     </div>
   );
 };
--- src/Componentnav/VoirMenu.jsx
+++ src/Componentnav/VoirMenu.jsx
@@ -5,11 +5,11 @@ import './VoirMenu.css';
 const VoirMenu = ({ onCategorySelect }) => {
   return (
     <div className="VoirMenu mt-5 w-100 text-center">
-      <button className='mt-5'> 
+      <button className='mt-5'>
         <span>Menu de </span>
-        <span><b>K</b>eur</span>
-        <span><b>Y</b>aay</span>
-        <span><b>S</b>oda</span>
+        <span style={{ margin: '0 5px' }}><b style={{color:"#91725d"}}>K</b>eur</span>
+        <span style={{ margin: '0 5px' }}><b style={{color:"#91725d"}}>Y</b>aay</span>
+        <span style={{ margin: '0 5px' }}><b style={{color:"#91725d"}}>S</b>oda</span>
       </button>
       <Section22 onCategorySelect={onCategorySelect} />
     </div>
