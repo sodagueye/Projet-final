5454234cdc87be63448dcc2cb85e71e1bf50fb12
Khadija <dija5631@gmail.com>
2024-06-13 11:46:28 +0200

    pull recette2

--- src/Dashboard/Card.jsx
+++ src/Dashboard/Card.jsx
@@ -33,33 +33,12 @@ export default function Card() {
             </div>
           </div>
         </div>
-<<<<<<< HEAD
-        <div className="col-lg-4  p-1">
-          <div class="card shadow carte p-2 ">
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content. Lorem ipsum dolor sit amet
-                accusantium tenetur ipsum
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4  p-1">
-          <div class="card shadow carte p-2">
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content. Lorem ipsum dolor sit amet
-                accusantium tenetur ipsum
-=======
         <div className="col-lg-3">
           <div class="card">
             <div class="card-body">
               <p class="card-text">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
->>>>>>> 274b1ed1e7c6f59d954e1455fc15beb29cee896b
               </p>
             </div>
           </div>
