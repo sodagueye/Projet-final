c98c55baf7c4ec33aa422a8111ad92ca3d02000b
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2024-06-24 12:01:45 +0200

    Page not found done!

--- src/App.jsx
+++ src/App.jsx
@@ -17,27 +17,32 @@ import ReservationTable from "./Reservation-Table/ReservationTable";
 import AppAjout from "./Ajouter/AppAjout";
 import Plats from "./Dashboard/Plats";
 import PlatsVendus from "./Dashboard/PlatsVendus";
+import PageNotFound from "./PageNotFound";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/reservation" element={<ReservationPage />} />
+          {/* User's Components */}
+          <Route path="/" element={<Accueil />} />
+          <Route path="/detail/:id" element={<Details />} />
+          <Route path="/menu" element={<Menu />} />
           <Route path="/inscription" element={<Inscrire />} />
           <Route path="/connexion" element={<Connexion />} />
           <Route path="/Modifier" element={<ModifierPassword />} />
           <Route path="/reinitialiser" element={<MotdepassOublié />} />
+          <Route path="/reservation" element={<ReservationPage />} />
+          {/* Admin's Components */}
           <Route path="/Utilisateur" element={<ListeUtilisateur />} />
-          <Route path="/" element={<Accueil />} />
-          <Route path="/ajout" element={<AppAjout />} />
+          <Route path="/admin" element={<Dashboard />} />
           <Route path="/employe" element={<Employes />} />
           <Route path="/table" element={<ReservationTable />} />
+          <Route path="/ventes" element={<PlatsVendus />} />
+          <Route path="/ajout" element={<AppAjout />} />
           <Route path="/update/:id" element={<Update />} />
           <Route path="/plats" element={<Plats />} />
-          <Route path="/admin" element={<Dashboard />} />
-          <Route path="/detail/:id" element={<Details />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/ventes" element={<PlatsVendus />} />
+          {/* Not Found Page */}
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PageNotFound() {
+  return (
+    <div className="d-flex justify-content-center align-items-center fs-1">
+      404 Not Found!
+    </div>
+  );
+}
