5222b57c8faf0d53a7604f7510c1ecc152067279
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2024-06-24 12:14:51 +0200

    Afficher le nombre de plats dispo

--- src/Dashboard/Card.jsx
+++ src/Dashboard/Card.jsx
@@ -3,15 +3,28 @@ import axios from "axios";
 import "./Cards.css";
 export default function Card() {
   const [plat, setPlat] = useState("");
-  
- 
+  async function Plats() {
+    try {
+      const response = await axios.get(
+        "https://tache-de-validition-nodejs-1p19n5070.vercel.app/admin/liste-produits"
+      );
+      const resultat = await response.data;
+      setPlat(resultat.length);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  useEffect(()=>{
+    Plats()
+  },[])
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-lg-3">
           <div class="card carte">
             <div class="card-body">
-              <p class="card-text"></p>
+              <p class="card-text">{plat}</p>
             </div>
           </div>
         </div>
