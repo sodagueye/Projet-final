036066d5061d8a850596771c6962c86bf16ee271
Khadija <dija5631@gmail.com>
2024-06-26 17:39:49 +0200

    testEmploy

--- src/Ressources-Humaines/AjoutEmployee.jsx
+++ src/Ressources-Humaines/AjoutEmployee.jsx
@@ -1,53 +1,54 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-
-function AddEmployee() {
-  const [name, setName] = useState("");
+const EmployeeForm = () => {
+  const [nom, setNom] = useState("");
   const [poste, setPoste] = useState("");
-  const [salary, setSalary] = useState("");
-  const [workHours, setWorkHours] = useState("");
-  const [monthlySalary, setMonthlySalary] = useState("");
+  const [salaire, setSalaire] = useState(0);
+  const [horaire, setHoraire] = useState(0);
+  const [mensualite,setMensualite] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const employee = {
-      name,
-      poste,
-      salary,
-      workHours,
-      monthlySalary,
+
+    const newEmployee = {
+      nom: nom,
+      poste: poste,
+      salaire: salaire,
+      horaire: horaire,
+      mensualite: mensualite
     };
-    axios
-      .post("http://localhost:5000/employees", employee)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-      
-      setTimeout(() => {
-        window.location.reload();
-      }, 3000);
+
+    try {
+      const response = await axios.post("https://tache-de-validition-nodejs-6.onrender.com/api/employes", newEmployee);
+      console.log("Employee added:", response.data);
+
+      setNom("");
+      setPoste("");
+      setSalaire(0);
+      setHoraire(0);
+      setMensualite("");
+    } catch (error) {
+      console.error("Error adding employee:", error);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3 row">
-        <label className="col-sm-3 col-form-label" for="name">Nom :</label>
+        <label className="col-sm-3 col-form-label" htmlFor="name">Nom :</label>
         <input
           className="form-control input"
           type="text"
           id="name"
           name="name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
+          value={nom}
+          onChange={(event) => setNom(event.target.value)}
+          required
         />
       </div>
       <div className="mb-3 row">
-        {" "}
-        <label className="col-sm-3 col-form-label" for="poste">Poste :</label>
+        <label className="col-sm-3 col-form-label" htmlFor="poste">Poste :</label>
         <input
           className="form-control input"
           type="text"
@@ -55,45 +56,53 @@ function AddEmployee() {
           name="poste"
           value={poste}
           onChange={(event) => setPoste(event.target.value)}
+          required
         />
       </div>
       <div className="mb-3 row">
-        <label className="col-sm-3 col-form-label" for="salaire">Salaire :</label>
+        <label className="col-sm-3 col-form-label" htmlFor="salary">Salaire :</label>
         <input
           className="form-control input"
           type="number"
           id="salary"
           name="salary"
-          value={salary}
-          onChange={(event) => setSalary(event.target.value)}
+          value={salaire}
+          onChange={(event) => setSalaire(event.target.value)}
+          required
         />
       </div>
       <div className="mb-3 row">
-        <label className="col-sm-3 col-form-label" for="workHours">Heure de travail :</label>
+        <label className="col-sm-3 col-form-label" htmlFor="workHours">Heure de travail :</label>
         <input
           className="form-control input"
           type="number"
           id="workHours"
           name="workHours"
-          value={workHours}
-          onChange={(event) => setWorkHours(event.target.value)}
+          value={horaire}
+          onChange={(event) => setHoraire(event.target.value)}
+          required
         />
       </div>
       <div className="mb-3 row">
-        <label className="col-sm-3 col-form-label" for="name">Mensualité :</label>
+        <label className="col-sm-3 col-form-label" htmlFor="monthlySalary">Mensualité :</label>
         <input
           className="form-control input"
           type="text"
           id="monthlySalary"
           name="monthlySalary"
-          value={monthlySalary}
-          onChange={(event) => setMonthlySalary(event.target.value)}
+          value={mensualite}
+          onChange={(event) => setMensualite(event.target.value)}
+          required
         />
       </div>
 
-      <button className="btn btn-submit" type="submit" value="Ajouter">AJouter</button>
+      <button className="btn btn-submit" type="submit">Ajouter</button>
     </form>
   );
-}
+};
 
-export default AddEmployee;
+export default EmployeeForm;
+
+
+
+
--- src/Ressources-Humaines/EmployeeList.jsx
+++ src/Ressources-Humaines/EmployeeList.jsx
@@ -1,99 +1,81 @@
-/* eslint-disable no-unused-vars */
-
-// Importation des bibliothèques nécessaires
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Employe.css"
+import "./Employe.css";
 
-// Fonction pour récupérer les données des employés
-const getEmployees = async () => {
-  const response = await axios.get("http://localhost:5000/employees");
-  return response.data;
-};
-
-// Composant EmployeeList
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([]);
   const [error, setError] = useState(null);
-  const [idToDelete, setIdToDelete] = useState(null);
+  const [formData, setFormData] = useState({
+    id: null,
+    nom: "",
+    poste: "",
+    salaire: 0,
+    horaire: 0,
+    mensualite: 0
+  });
 
   useEffect(() => {
-    getEmployees()
-      .then((data) => {
-        setEmployees(data);
-      })
-      .catch((error) => {
-        setError(error);
-      });
+    getEmployees();
   }, []);
 
-  // Button pour supprimer un employé
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/employees')
-      .then((response) => {
-        setEmployees(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  const handleDelete = (id) => {
-    axios.delete(`http://localhost:5000/employees/${id}`)
-      .then((response) => {
-        setEmployees(employees.filter((employee) => employee.id !== id));
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  const getEmployees = async () => {
+    try {
+      const response = await axios.get("https://tache-de-validition-nodejs-6.onrender.com/api/employes/getting");
+      setEmployees(response.data);
+    } catch (error) {
+      setError(error);
+    }
   };
-  return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>Nom</th>
-          <th>Poste</th>
-          <th>Salaire</th>
-          <th>Heure de travail</th>
-          <th>Mensualité</th>
-          <th>Supprimer</th>
-        </tr>
-      </thead>
-      <tbody>
-        {employees.map((employee) => (
-          <tr key={employee.id}>
-            <td>{employee.name}</td>
-            <td>{employee.poste}</td>
-            <td>{employee.salary}</td>
-            <td>{employee.workHours}</td>
-            <td>{employee.monthlySalary}</td>
-            <td>
-              <div>
-                <button
-                  key={employee.id}
-                  className="btn btn-danger"
-                  onClick={() => setIdToDelete(employee.id)}
-                >
-                  Supprimer
-                </button>
+    //  ********CA MARCHE PAS ENCORE*********//
 
-                {idToDelete && (
-                  <button
-                    className="btn btn-success"
-                    onClick={() => handleDelete(idToDelete)}
-                  >
-                    Confirmer suppression
-                  </button>
-                )}
-              </div>
-            </td>
+  // const handleDeleteEmployee = async (id) => {
+  //   try {
+  //     // Envoyer la requête DELETE pour supprimer l'employé
+  //     await axios.delete(`https://tache-de-validition-nodejs-6.onrender.com/api/employes/delete/${id}`);
+  
+  //     // Mise à jour de la liste des employés localement après la suppression
+  //     setEmployees(employees.filter(employee => employee.id !== id));
+  
+  //     console.log(`Employé avec l'id ${id} supprimé avec succès.`);
+  
+  //   } catch (error) {
+  //     console.error("Erreur de Suppression:", error);
+  //     // Gestion des erreurs ici, par exemple, affichage d'un message d'erreur à l'utilisateur
+  //   }
+  // };
+  
+
+  return (
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>Poste</th>
+            <th>Salaire</th>
+            <th>Heures de travail</th>
+            <th>Mensualité</th>
+            <th>Actions</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.nom}</td>
+              <td>{employee.poste}</td>
+              <td>{employee.salaire}</td>
+              <td>{employee.horaire}</td>
+              <td>{employee.mensualite}</td>
+              <td>
+                {/* <button onClick={() => handleDeleteEmployee(employee.id)}>Supprimer</button> */}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-// Export du component
 export default EmployeeList;
+
