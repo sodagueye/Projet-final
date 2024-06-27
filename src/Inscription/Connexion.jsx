26d1696cd014e98a28750427e7d2f06ab0243ec4
Khadija <dija5631@gmail.com>
2024-06-27 16:17:58 +0200

    FinAuth

--- src/Inscription/Connexion.jsx
+++ src/Inscription/Connexion.jsx
@@ -1,74 +1,94 @@
-import React, { useState } from 'react';
-import '../Inscription/inscrire.css';
-import { Link, useNavigate } from 'react-router-dom';
-// import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import logoMaron from '../assets/logoMaron.png';
+import React, { useState } from "react";
+import "../Inscription/inscrire.css";
+import { Link, useNavigate} from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import logoMaron from "../assets/logoMaron.png";
 
 function Connexion() {
-     const navigate = useNavigate();
-   
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    async function login(e) {
-        e.preventDefault();
-        try {
-            const res = await axios.post("https://tache-de-validition-nodejs-6.onrender.com/api/auth", {
-                email,
-                password,
-            });
-
-            if (res.data === "exist") {
-                toast.success("Connexion réussie");
-                // history("/")
-                console.log("Redirection vers la page d'accueil...");
-                 navigate("/"); 
-            } else {
-                toast.error("Email ou mot de passe incorrect");
-            }
-
-            setEmail('');
-            setPassword('');
-
-        } catch (error) {
-            console.error(error);
-            toast.error("Erreur lors de la connexion");
+  async function login(e) {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "https://tache-de-validition-nodejs-6.onrender.com/api/auth",
+        {
+          email,
+          password,
         }
+      );
+      if (res.status === 201) {
+        setEmail("");
+        setPassword("");
+        toast.success("Connexion réussie.");
+        setTimeout(() =>{
+            navigate("/");
+          }, 2000);
+        }
+      else {
+        toast.error(res.data.errors[0].msg);
+      }
+    } catch (error) {
+      toast.error("Erreur lors de l'inscription");
+      console.error(error);
     }
+  }
 
-    return (
-        <div>
-            <div className='backCConnexion shadow mt-5'>
-                <form className='form align-items-center' onSubmit={login}>
-                    <img src={logoMaron} className='fs-2 logoMaron' alt="" />
-                    <h2 className='text-center fw-bold fs-2 color'>Connecter</h2>
+  return (
+    <div>
+      <div className="backCConnexion shadow mt-5">
+        <form className="form align-items-center" onSubmit={login}>
+          <img src={logoMaron} className="fs-2 logoMaron" alt="" />
+          <h2 className="text-center fw-bold fs-2 color">Connecter</h2>
 
-                    <div className='inscript1'>
-                        <input className="nom email" type="email" placeholder="Email" required onChange={(e) => setEmail(e.target.value)} />
-                        <input className="nom email" type="password" placeholder="mot de passe" required onChange={(e) => setPassword(e.target.value)} />
-                    </div>
+          <div className="inscript1">
+            <input
+              className="nom email"
+              type="email"
+              placeholder="Email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className="nom email"
+              type="password"
+              placeholder="mot de passe"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-                    <div className='text-center my-4'>
-                        <Link to="/reinitialiser" className='oublie'>mot de passe oublié</Link>
-                    </div>
+          <div className="text-center my-4">
+            <Link to="/reinitialiser" className="oublie">
+              mot de passe oublié
+            </Link>
+          </div>
 
-                    <div className='creer'>
-                        <button type='submit' className='liens align-items-center connect fs-5 fw-bold'>Se connecter</button>
-                    </div>
+          <div className="creer">
+            <button
+              type="submit"
+              className="liens align-items-center connect fs-5 fw-bold"
+            >
+              Se connecter
+            </button>
+          </div>
 
-                    <p className='text-center mt-3'>Vous n'avez pas de compte ?</p>
+          <p className="text-center mt-3">Vous n'avez pas de compte ?</p>
 
-                    <div className='text-center my-4'>
-                        <Link to="/inscription" className='oublie'>Créer un compte</Link>
-                    </div>
-                </form>
-            </div>
-            <ToastContainer position='top-center' />
-        </div>
-    );
+          <div className="text-center my-4">
+            <Link to="/inscription" className="oublie">
+              Créer un compte
+            </Link>
+          </div>
+        </form>
+      </div>
+      <ToastContainer position="top-center" />
+    </div>
+  );
 }
 
 export default Connexion;
--- src/Inscription/Inscrire.jsx
+++ src/Inscription/Inscrire.jsx
@@ -1,117 +1,19 @@
-// import React, { useState } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
-// import axios from 'axios';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import logoMaron from '../assets/logoMaron.png';
-// import './inscrire.css'; // Assurez-vous que votre CSS est correctement importé ici
-
-// function Inscrire() {
-//   const navigate = useNavigate();
-//   const [firstName, setFirstName] = useState('');
-//   const [lastName, setLastName] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [number, setNumber] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [confirmPassword, setConfirmPassword] = useState('');
-
-//   async function submit(e) {
-//     e.preventDefault();
-
-//     if (password !== confirmPassword) {
-//       toast.error("Les mots de passe ne correspondent pas");
-//       return;
-//     }
-//     try {
-//       const res = await axios.post("https://tache-de-validition-nodejs-6.onrender.com/api/register", {
-//         firstName,
-//         lastName,
-//         email,
-//         number,
-//         password,
-//         confirmPassword
-//       });
-
-//       if (res.data === "Inscription réussie. Un email de confirmation a été envoyé à votre adresse.") {
-//         toast.success("Inscription réussie");
-//         // Réinitialiser les champs du formulaire après une inscription réussie
-//         setFirstName('');
-//         setLastName('');
-//         setEmail('');
-//         setNumber('');
-//         setPassword('');
-//         setConfirmPassword('');
-//         // Rediriger vers la page de connexion après une inscription réussie
-//         navigate('/connexion');
-//       }
-
-//      else if (res.data === "exist") {
-//         toast.error("L'utilisateur existe déjà");
-//       }
-//        else {
-//         toast.error("Erreur lors de l'inscription");
-//       }
-//     } catch (error) {
-//       toast.error("Erreur lors de l'inscription");
-//       console.error(error);
-//     }
-//   }
-
-//   return (
-//     <div className=''>
-//       <div className='back shadow d-flex justify-content-center'>
-//         <form className='form' onSubmit={submit}>
-//           <img src={logoMaron} className='fs-2 logoMaron' alt="" />
-//           <div className='color m-0 fw-bold'>
-//             <h3>Créez votre compte</h3>
-//           </div>
-//           <div className='auth'>
-//             <input className="nom" type="text" style={{ backgroundColor: "transparent" }} placeholder="Prénom" required value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-//             <input className="nom number" type="text" placeholder="Nom" required value={lastName} onChange={(e) => setLastName(e.target.value)} />
-//           </div>
-//           <div className='auth'>
-//             <input className="nom" type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
-//             <input className="nom number" type="number" placeholder="Téléphone" required value={number} onChange={(e) => setNumber(e.target.value)} />
-//           </div>
-//           <div className='auth pass'>
-//             <input className="nom" type="password" placeholder="Mot de passe" required value={password} onChange={(e) => setPassword(e.target.value)} />
-//             <input className="nom" type="password" placeholder="Confirmation mot de passe" required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-//           </div>
-//           <div className='creer'>
-//             <button type='submit' className='creer liens fs-5 fw-bold mt-3 connect'>Créer un compte</button>
-//           </div>
-//         </form>
-//       </div>
-//       <ToastContainer position='top-center' />
-//     </div>
-//   )
-// }
-
-// export default Inscrire;
-
-
-
-
-
-
-
-
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import logoMaron from '../assets/logoMaron.png';
-import './inscrire.css'; 
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import logoMaron from "../assets/logoMaron.png";
+import "./inscrire.css";
 
 function Inscrire() {
   const navigate = useNavigate();
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [number, setNumber] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   async function submit(e) {
     e.preventDefault();
@@ -122,64 +24,117 @@ function Inscrire() {
     }
 
     try {
-      const res = await axios.post("https://tache-de-validition-nodejs-3.onrender.com/api/register", {
-        firstName,
-        lastName,
-        email,
-        number,
-        password,
-        confirmPassword
-      });
+      const res = await axios.post(
+        "https://tache-de-validition-nodejs-6.onrender.com/api/register",
+        {
+          firstName,
+          lastName,
+          email,
+          number,
+          password,
+          confirmPassword,
+        }
+      );
 
       if (res.status === 201) {
-        toast.success(res.data.msg);
-        setFirstName('');
-        setLastName('');
-        setEmail('');
-        setNumber('');
-        setPassword('');
-        setConfirmPassword('');
-        navigate('/connexion');
-      }
-       else {
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setNumber("");
+        setPassword("");
+        setConfirmPassword("");
+        toast.success(
+        "Inscription réussie."
+        );
+        setTimeout(() =>{
+          navigate("/connexion");
+        }, 2000);
+       
+      } else {
         toast.error(res.data.errors[0].msg);
       }
     } catch (error) {
       toast.error("Erreur lors de l'inscription");
-      // toast.success("Vous etes inscrite");
       console.error(error);
     }
   }
 
   return (
-    <div className=''>
-      <div className='back shadow d-flex justify-content-center'>
-        <form className='form' onSubmit={submit}>
-          <img src={logoMaron} className='fs-2 logoMaron' alt="" />
-          <div className='color m-0 fw-bold'>
+    <div className="">
+      <div className="back shadow d-flex justify-content-center">
+        <form className="form" onSubmit={submit}>
+          <img src={logoMaron} className="fs-2 logoMaron" alt="" />
+          <div className="color m-0 fw-bold">
             <h3>Créez votre compte</h3>
           </div>
-          <div className='auth'>
-            <input className="nom" type="text" style={{ backgroundColor: "transparent" }} placeholder="Prénom" required value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-            <input className="nom number" type="text" placeholder="Nom" required value={lastName} onChange={(e) => setLastName(e.target.value)} />
+          <div className="auth">
+            <input
+              className="nom"
+              type="text"
+              style={{ backgroundColor: "transparent" }}
+              placeholder="Prénom"
+              required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              className="nom number"
+              type="text"
+              placeholder="Nom"
+              required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
           </div>
-          <div className='auth'>
-            <input className="nom" type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input className="nom number" type="number" placeholder="Téléphone" required value={number} onChange={(e) => setNumber(e.target.value)} />
+          <div className="auth">
+            <input
+              className="nom"
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className="nom number"
+              type="number"
+              placeholder="Téléphone"
+              required
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+            />
           </div>
-          <div className='auth pass'>
-            <input className="nom" type="password" placeholder="Mot de passe" required value={password} onChange={(e) => setPassword(e.target.value)} />
-            <input className="nom" type="password" placeholder="Confirmation mot de passe" required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+          <div className="auth pass">
+            <input
+              className="nom"
+              type="password"
+              placeholder="Mot de passe"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              className="nom"
+              type="password"
+              placeholder="Confirmation mot de passe"
+              required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
           </div>
-          <div className='creer'>
-            <button type='submit' className='creer liens fs-5 fw-bold mt-3 connect'>Créer un compte</button>
+          <div className="creer">
+            <button
+              type="submit"
+              className="creer liens fs-5 fw-bold mt-3 connect"
+            >
+              Créer un compte
+            </button>
           </div>
         </form>
       </div>
-      <ToastContainer position='top-center' />
+      <ToastContainer position="top-center" />
     </div>
-  )
+  );
 }
 
 export default Inscrire;
-
