eb128c047783be0e80cf5a5b07a1382cff80cc69
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2024-07-09 11:21:21 +0200

    new push

--- src/Componentnav/PlatSenegalais.jsx
+++ src/Componentnav/PlatSenegalais.jsx
@@ -29,7 +29,6 @@ export default function PlatSenegalais() {
     : products;
 
   return (
-<<<<<<< HEAD
     // <div>
     //       <div className='row'>
     //         {filteredProducts.map((item) => (
@@ -63,21 +62,6 @@ export default function PlatSenegalais() {
             >
               <span className="visually-hidden">Loading...</span>
             </div>
-=======
-    <div className='container'>
-          <div className='row justify-content-center'>
-            {filteredProducts.map((item) => (
-             <div key={item.id} className="col-lg-4 mt-4 scale-image">
-               <Link to={`/detail/${item._id}`}>
-                 <div className="card border-0 carte shadow">
-                   <img src={item.image} className="card-img-top" alt="..."/>   
-                   {/* <h3>{item.name}</h3>              */}
-                 </div>
-               </Link>
-             </div>
-           ))
-          }
->>>>>>> 3cb5c14d1462b4dfb2b933a3f002448112b97a08
           </div>
         ) : (
           <div className="row mt-4">
