56111bcb36d805e9cfb90f6578d22a820b0b9a16
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2024-07-09 14:08:25 +0200

    predeploy

--- src/Componentnav/MenuBtn.jsx
+++ src/Componentnav/MenuBtn.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 export default function MenuBtn() {
   return (
     <div className="navmenu d-flex justify-content-center align-items-center pt-5 mt-5">
-      <NavLink to="/menu/tout">Tout</NavLink>
-      <NavLink to="/menu/platsSenegalais">Plats Sénégalais</NavLink>
-      <NavLink to="/menu/burgers">FastFood</NavLink>
-      <NavLink to="/menu/cuisineAilleurs">Cuisine D'ailleurs</NavLink>
-      <NavLink to="/menu/boissons"> Boissons</NavLink>
-      <NavLink to="/menu/desserts">Desserts</NavLink>
+      <NavLink className="text-white shadow" to="/menu/tout">Tout</NavLink>
+      <NavLink className="text-white shadow" to="/menu/platsSenegalais">Plats Sénégalais</NavLink>
+      <NavLink className="text-white shadow" to="/menu/burgers">FastFood</NavLink>
+      <NavLink className="text-white shadow" to="/menu/cuisineAilleurs">Cuisine D'ailleurs</NavLink>
+      <NavLink className="text-white shadow" to="/menu/boissons"> Boissons</NavLink>
+      <NavLink className="text-white shadow" to="/menu/desserts">Desserts</NavLink>
     </div>
   );
 }
