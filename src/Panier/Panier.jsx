b32bf4fb47a97066e415716ac8230ebced5df779
boubacar26 <boubacar.ndiaye.me@gmail.com>
2024-07-10 16:04:26 +0000

    Test

--- src/Panier/Panier.jsx
+++ src/Panier/Panier.jsx
@@ -3,18 +3,16 @@ import { Context } from "../Components";
 import { MdOutlineDelete } from "react-icons/md";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
 
 const Panier = () => {
-  const { cartItems, setCartItems, setCartQuantity,totalPrix, handleClick } = useContext(Context);
-
-  // const totalPrix = cartItems.reduce((total, item) => {
-  //   return total + (item.price || 0);
-  // }, 0);
+  const { cartItems, setCartItems, setCartQuantity, totalPrix, handleClick } =
+    useContext(Context);
 
   const handleDelete = (id) => {
     const newCartItems = cartItems.filter((item) => item._id !== id);
     setCartItems(newCartItems);
-    toast.success("Le produit a été retiré du panier");
+    toast.success("Le produit a été retiré du panier", { autoClose: 1000 });
     setCartQuantity(newCartItems.length);
     localStorage.setItem("cartItems", JSON.stringify(newCartItems));
   };
@@ -25,17 +23,11 @@ const Panier = () => {
       setCartItems(savedCartItems);
     }
   }, [setCartItems]);
-  // const handleClick = () => {
-  //   if (totalPrix === 0) {
-  //     toast.error("Veuillez choisir des plats");
-  //   } else {
-  //     toast.success("Commande validée avec succès");
-  //   }
-  // };
+
   return (
     <div className="container-fluid test">
       <div className="row">
-        <div className="col-lg-9 bg-body shadow rounded">
+        <div className="col-lg-9 bg-body shadow rounded p-4">
           {cartItems.length > 0 ? (
             <div>
               <h3 className="text-start">Panier ({cartItems.length})</h3>
@@ -45,15 +37,14 @@ const Panier = () => {
                   <div className="d-flex align-items-center justify-content-between">
                     <div className="d-flex justify-content-center align-items-center">
                       <div>
-                        <img className="product-img" src={`https://tache-de-validition-nodejs-6.onrender.com/${item.image}`} alt="" />
-                        <div>
-                          <span
+                        <img className="product-img" src={item.image} alt="" />
+                        <div className="d-flex justify-content-between">
+                          <div
                             onClick={() => handleDelete(item._id)}
                             className="fs-3 cart"
                           >
                             <MdOutlineDelete />
-                          </span>
-                          <span className="delete fw-bold fs-6">SUPPRIMER</span>
+                          </div>
                         </div>
                       </div>
 
@@ -69,8 +60,8 @@ const Panier = () => {
             <p>Votre panier est vide.</p>
           )}
         </div>
-        <div className="col-lg-3">
-          <div className="card shadow">
+        <div className="col-lg-3 p-2">
+          <div className="card shadow border-0">
             <div className="card-body">
               <h5 className="card-title text-uppercase text-start">
                 Résumé du panier
@@ -84,7 +75,7 @@ const Panier = () => {
                 onClick={handleClick}
                 className="btn w-100 btn-commande text-white shadow"
               >
-                Commander ({totalPrix} FCFA)
+                <Link to="connexion">Commander ({totalPrix} FCFA)</Link>
               </button>
             </div>
           </div>
