dbc3ae77a0bfb89f2176a065101616bbbb78fa3f
DELL LATITUDE 7470 <gueyesoda56@gmail.com>
2024-07-10 12:41:21 +0000

    modifier sidebar

--- src/Dashboard/Sidebar.jsx
+++ src/Dashboard/Sidebar.jsx
@@ -11,38 +11,38 @@ import "../Dashboard/Sidbar.css"
 export default function Sidebar() {
   return <div className="sidbar shadow text-white">
     <div className="d-flex flex-column justify-content-around align-items-start fs-5 div">
-      <NavLink to="/admin" activeClassName="active">
-        <div className="d-flex justify-content-start align-items-center py-2 ms-2 das">
+      <NavLink to="/admin" className={({ isActive }) => (isActive ? "active" : "")}>
+        <div className="d-flex justify-content-start align-items-center py-2 p-2 das gap-2 m-0">
           <AiFillDashboard />
           <span className="das">Dashboard</span>
         </div>
       </NavLink>
       <NavLink to="/admin/utilisateurs" className={({ isActive }) => (isActive ? "active" : "")}>
-        <div className="d-flex justify-content-start align-items-center ms-2 das">
+        <div className="d-flex justify-content-start align-items-center  das gap-2 m-0">
           <CiUser />
           <span>Utilisateurs</span>
         </div>
       </NavLink>
       <NavLink to="/admin/plats" className={({ isActive }) => (isActive ? "active" : "")}>
-        <div className="d-flex justify-content-start align-items-center  ms-2 das">
+        <div className="d-flex justify-content-start align-items-center   das gap-2 m-0">
           <GiHotMeal />
           <span> Plats</span>
         </div>
       </NavLink>
       <NavLink to="/admin/employe" className={({ isActive }) => (isActive ? "active" : "")}>
-        <div className="d-flex justify-content-start align-items-center  ms-2 das">
+        <div className="d-flex justify-content-start align-items-center m-2  das gap-2 m-0">
           <GiHumanTarget />
-          <span>R_H</span>
+          <span>RH</span>
         </div>
       </NavLink>
       <NavLink to="/admin/ventes" className={({ isActive }) => (isActive ? "active" : "")}>
-        <div className="d-flex justify-content-start align-items-center  ms-2 das">
+        <div className="d-flex justify-content-start align-items-center m-2  das gap-2 m-0">
           <FaSellsy />
           <span className="">Ventes</span>
         </div>
       </NavLink>
       <NavLink to="/admin/commande" className={({ isActive }) => (isActive ? "active" : "")}>
-        <div className="d-flex justify-content-start align-items-center  ms-2 das">
+        <div className="d-flex justify-content-start align-items-center m-2   das gap-2">
           <FaSellsy />
           <span>Commande</span>
         </div>
